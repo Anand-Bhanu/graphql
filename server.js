ea2bfc7dde8ce6c6200e775135ff916ba1f5152e
Anand-Bhanu <anandbhanu33@gmail.com>
2020-06-11 10:47:30 +0530

    Update server.js
    Added the Cost Analysis and Rate Limit plugins.

--- server.js
+++ server.js
@@ -29,3 +29,54 @@ app.get('/playground',
 app.listen({ port: PORT }, () => {
     console.log('Server listening on port 4000')
 });
+
+const costAnalysis = require('graphql-cost-analysis').default
+const costAnalyzer = costAnalysis({
+    maximumCost: 1000,
+  })
+  
+const graphqlExpress = require('express');
+app.use(
+  '/graphql',
+  graphqlExpress(req => {
+    return {
+      schema,
+      rootValue: null,
+      validationRules: [
+        costAnalysis({
+          variables: req.body.variables,
+          maximumCost: 1000,
+        }),
+      ],
+    }
+  })
+)
+
+const createRateLimitRule = require('graphql-rate-limit').default
+const shield = require('graphql-shield');
+
+// Step 1: get rate limit shield instance rule
+const rateLimitRule = createRateLimitRule({ identifyContext: (ctx) => ctx.id });
+
+const permissions = shield({
+  Query: {
+    // Step 2: Apply the rate limit rule instance to the field with config
+    getItems: new rateLimitRule({ window: "1s", max: 5 })
+  }
+});
+
+const schema = applyMiddleware(
+  makeExecutableSchema({
+    typeDefs: gql`
+      type Query {
+        getItems: [Item]
+      }
+    `,
+    resolvers: {
+      Query: {
+        getItems: () => [{ id: '1' }]
+      }
+    }
+  }),
+  permissions
+)
